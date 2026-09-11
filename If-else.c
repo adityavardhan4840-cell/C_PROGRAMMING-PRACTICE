4ffbf40247c433899491f6ad7684b35c7445990e
ADITYA <adityavardhan4840@gmail.com>
2026-09-11 13:17:46 +0530

    This c program about one of the conditional operator "IF-ELSE" .

---
+++ If-else.c
@@ -0,0 +1,10 @@
+/* IF-ELSE */
+#include<stdio.h>
+void main()
+{
+int num;
+printf("Enter a number : ");
+scanf("%d", &num);
+if (num % 2 == 0){ printf("The number is : Even ");}
+else {printf("The number is : Odd ");}
+}
